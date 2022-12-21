ce5553dc6381a6528cfea4be558e8ad11a57f097
Iggynp <odissey0x@gmail.com>
2022-12-21 14:27:20 +0100

    useState

--- calculator/src/components/CalculatorBox.jsx
+++ calculator/src/components/CalculatorBox.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "../styles/CalculatorBoxStyles.css";
 import { CalculatorBtn } from "./CalculatorBtn";
 import { CalculatorScreen } from "./CalculatorScreen";
 
 export const CalculatorBox = () => {
 
+	const [screenValue, setScreenValue] = useState(0);
+
 	return (<div className="calculatorBox--skeleton">
-		<CalculatorScreen></CalculatorScreen>
+		<CalculatorScreen{screenValue}></CalculatorScreen>
 		<CalculatorBtn></CalculatorBtn>
 		</div>);
 }
