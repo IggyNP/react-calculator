6f2d69166662d55701f0f8a91339dc3a18902ec0
Iggynp <odissey0x@gmail.com>
2022-12-29 20:14:39 +0100

    adding switches for ops

--- calculator/src/components/CalculatorBox.jsx
+++ calculator/src/components/CalculatorBox.jsx
@@ -4,14 +4,78 @@ import { CalculatorBtn } from "./CalculatorBtn";
 import { CalculatorScreen } from "./CalculatorScreen";
 
 export const CalculatorBox = () => {
+  const [screenValue, setScreenValue] = useState("");
 
-	const [screenValue, setScreenValue] = useState(0);
-
-	return (<div className="calculatorBox--skeleton">
-		<CalculatorScreen screenValue = {screenValue}></CalculatorScreen>
-		<CalculatorBtn digit = {7} setScreenValue = {setScreenValue}> </CalculatorBtn>
-		<CalculatorBtn> digit = {7} setScreenValue = {setScreenValue} </CalculatorBtn>
-		<CalculatorBtn> digit = {7} setScreenValue = {setScreenValue} </CalculatorBtn>
-		<CalculatorBtn> digit = {7} setScreenValue = {setScreenValue} </CalculatorBtn>
-		</div>);
-}
+  return (
+    <div className="calculatorBox--skeleton">
+      <CalculatorScreen screenValue={screenValue}></CalculatorScreen>
+      <div className="row.1">
+        <CalculatorBtn digit={"C"} setScreenValue={setScreenValue} screenValue={screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"DEL"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"%"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"/"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+      </div>
+	  <div className="row.2">
+        <CalculatorBtn digit={"7"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"8"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"9"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"*"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+      </div>
+	  <div className="row.3">
+        <CalculatorBtn digit={"4"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"5"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"6"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"-"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+      </div>
+	  <div className="row.4">
+        <CalculatorBtn digit={"1"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"2"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"3"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"+"} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+      </div>
+	  <div className="row.0">
+        <CalculatorBtn digit={"0"} setScreenValue={setScreenValue} screenValue = {screenValue}> 
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"."} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+        <CalculatorBtn digit={"="} setScreenValue={setScreenValue} screenValue = {screenValue}>
+          {" "}
+        </CalculatorBtn>
+      </div>
+    </div>
+  );
+};
--- calculator/src/components/CalculatorBtn.jsx
+++ calculator/src/components/CalculatorBtn.jsx
@@ -1,9 +1,26 @@
-export const CalculatorBtn = ({ digit, setScreenValue }) => {
+export const CalculatorBtn = ({ digit, setScreenValue, screenValue }) => {
+
+	const handleClickBtn = (element) => {
+		switch(element){
+			case "C":
+				setScreenValue('')
+				break;
+			case "DEL":
+				setScreenValue(screenValue.slice(0, -1))
+				break;
+			case "=":
+				// eslint-disable-next-line
+				setScreenValue(eval(screenValue))
+				break;
+			default:
+				setScreenValue(screenValue + element)
+		}
+	}
   return (
     <button
       className="calculatorBox--skeleton-btn"
       onClick={() => {
-        setScreenValue(digit);
+       handleClickBtn(digit)
       }}
     >
       {digit}
